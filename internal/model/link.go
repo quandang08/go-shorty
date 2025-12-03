5f8a05aefaabb6c1c485357b63ad50eba11fa7c8
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-12-03 22:38:27 +0700

    feat: Implement DTOs, install Gin/Config dependencies, and create config files

--- internal/model/link.go
+++ internal/model/link.go
@@ -13,3 +13,17 @@ type Link struct {
 
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
+
+// CreateLinkRequest DTO nhận vào khi tạo link
+type CreateLinkRequest struct {
+	OriginalURL string `json:"original_url" binding:"required"`
+}
+
+// LinkResponse DTO trả về cho client
+type LinkResponse struct {
+	ShortCode   string    `json:"short_code"`
+	OriginalURL string    `json:"original_url"`
+	ClicksCount uint      `json:"clicks_count"`
+	CreatedAt   time.Time `json:"created_at"`
+	ShortURL    string    `json:"short_url"`
+}
